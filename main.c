cf4b44eb2671b99abeaa63f72c120e1d23c7175d
Fábián Gábor <info@fabiangabor.com>
2019-11-16 22:45:28 +0100

    mergeSort

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct user {
     char *username;
@@ -56,6 +57,57 @@ struct user *find_adults(struct user *data, int n)
     return d;
 }
 
+int *mergeSort(int *a, int n1, int *b, int n2)
+{
+    static int *array; // static variable lenght array?!
+    array = malloc((n1+n2) * sizeof(int));
+
+    int i=0;
+    int j=0;
+    int k=0;
+
+    while ((i<n1) && (j<n2))
+    {
+        if (a[i]<b[j])
+        {
+            array[k]=a[i];
+            i++;
+        }
+        else
+        {
+            array[k]=b[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i<n1)
+    {
+        array[k]=a[i];
+        i++;
+        k++;
+    }
+    while (j<n2)
+    {
+        array[k]=b[j];
+        j++;
+        k++;
+    }
+
+    return array;
+}
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+void printArray(int *a, int n)
+{
+    for (int i=0; i<n; i++)
+         printf("%d ", a[i]);
+    printf("\n\n");
+}
+
 int main()
 {
     // 1. Készíts egy 10 elemű, egész számokat tartalmazó tömböt értékekkel! Állíts be egy pointer a tömb ötödik elemére!
@@ -79,12 +131,47 @@ int main()
     //bubble_sort(user,n);
 
     // 4. Készíts függvényt, ami a felhasználók tömbjéből kiválogatja azokat, akik nagykorúak (betöltötték a 18-at)!
-    // A függvény térjen vissza az eredménytömb címével!    
+    // A függvény térjen vissza az eredménytömb címével!
+    /*
     struct user *adults = find_adults(user,n);
 
     printf("Adults:\n");
     for (int i=0; adults[i].username != NULL; i++)
         printf("%10s %d\n", adults[i].username, adults[i].bday);
+    */
+
+    // 5. Készíts függvényt, ami két paraméterül kapott, rendezett egészekből álló tömböt összefűz egyetlen új, rendezett tömbbe!
+    // A függvény térjen vissza az új tömb kezdőcímével!
+    int a[4];
+    int b[6];
+    int size_a = sizeof(a)/sizeof(a[0]);
+    int size_b = sizeof(b)/sizeof(b[0]);
+
+    int *array;
+    for (int i=0; i<size_a; i++)
+        a[i] = rand() % 50;
+    for (int i=0; i<size_b; i++)
+        b[i] = rand() % 50;
+
+    printf("size_a: %d\n",size_a);
+    printf("Unsorted a:\n");
+    printArray(a, size_a);
+
+    printf("size_b: %d\n",size_b);
+    printf("Unsorted b:\n");
+    printArray(b, size_b);
+
+    qsort(a, sizeof(a)/sizeof(a[0]), sizeof(a[0]), cmpfunc);
+    qsort(b, sizeof(b)/sizeof(b[0]), sizeof(b[0]), cmpfunc);
+
+    printf("Sorted a:\n");
+    printArray(a, size_a);
+    printf("Sorted b:\n");
+    printArray(b, size_b);
+
+    array = mergeSort(a, size_a, b, size_b);
+    printf("Sorted merged:\n");
+    printArray(array, size_a + size_b);
 
     return 0;
 }
